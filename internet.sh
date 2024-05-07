ce413fb07c282eb20202325e34bf7dc96282956f
gabriel carvalho <92344473+gabrieloak123@users.noreply.github.com>
2024-05-07 20:38:32 +0000

    Update internet.sh

--- internet.sh
+++ internet.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 nmcli r wifi on
-nmcli d wifi connect Casa16 password 30091938
+nmcli r wifi list
+
+echo "Type the network you want to connect"
+read network
+echo "Type the password"
+read password
+
+nmcli d wifi connect $network password $password
 sleep 10
 nmcli d
